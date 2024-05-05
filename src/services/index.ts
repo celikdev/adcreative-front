635f164fcd1baec4a0960f63bbf2712d266f96c7
Hakan <52114496+celikdev@users.noreply.github.com>
2024-05-05 19:21:38 +0300

    fix: vercel deployment type error

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,5 +2,6 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const ClientBase = createApi({
     baseQuery: fetchBaseQuery({ baseUrl: "https://rickandmortyapi.com/api" }),
+    // @ts-ignore
     endpoints: (builder) => ({}),
 })
